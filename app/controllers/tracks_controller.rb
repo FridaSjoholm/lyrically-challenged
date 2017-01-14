949a559285435efe930c4a2cd721819559e46129
SalasC2 <christiansalas96@gmail.com>
2017-01-14 12:19:25 -0800

    Add loading screen rendering and then display data after response is receieved

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -6,16 +6,14 @@ class TracksController < ApplicationController
 
   def search
     @word = params[:word]
-    url = "http://api.musicgraph.com/api/v2/track/search?api_key=" + ENV['MUSIC_GRAPH_API_KEY'] + "&lyrics_phrase=" + @word
+    url = "http://api.musicgraph.com/api/v2/track/search?api_key=" + ENV['MUSIC_GRAPH_API_KEY'] + "&lyrics_phrase=" + @word + "&limit=100"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     @tracks = JSON.parse(response)
 
     respond_to do |format|
       if @tracks["data"].length > 0
-        format.html {render :show}
-        format.js {}
-        format.json {}
+        format.html {render :show, layout: false}
       else
         flash[:danger] = 'There was a problem'
         format.html { render :index }
