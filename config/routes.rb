78a72f3abe4fb2352a964b0d7e8e9222636ed650
Frida Sjoholm <frida@fridaesign.se>
2017-01-13 17:16:43 -0800

    Adds search form to startpage using ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tracks#index'
-  resources :tracks, only: [:index]
-
+  resources :tracks, only: [:index, :search, :show ]
+  get "/search", to: "tracks#search"
 end
