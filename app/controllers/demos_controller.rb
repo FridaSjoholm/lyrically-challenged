2d1504615c90b37ba1ab72761a4d0e82fc6ab8d5
kattak <katrina.devaney@gmail.com>
2017-01-13 19:30:00 -0800

    working call to lyricapi

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -1,30 +1,19 @@
 class DemosController < ApplicationController
 
   def index
-    #Sample call to lololyrics
+    #Sample call to lyricapi
     #http://api.lololyrics.com/0.5/getLyric?artist=Rihanna+J&track=Umbrella
 
-    # http://localhost:3000/demos?song=Umbrella&artist=Rihanna
+    # https://lyric-api.herokuapp.com/api/find/artist/song
 
     #set variables
     @song = params[:song]
     @artist = params[:artist]
 
     #Make call to lololyrics API
-    url = "http://api.lololyrics.com/0.5/getLyric?artist=#{@artist}+J&track=#{@song}"
+    url = "https://lyric-api.herokuapp.com/api/find/#{@artist}/#{@song}"
     uri = URI(url)
-    response = Net::HTTP.get(uri)
+  p  @lyrics = Net::HTTP.get(uri)
 
-    #Parse response with Nokogiri
-    p noko = Nokogiri::XML.parse(response)
-    p @lyrics = noko.children.children.children[1]
   end
-
-
-#For whitespaces, tried:
-# &#160
-# %20
-# + 
-
-
 end
