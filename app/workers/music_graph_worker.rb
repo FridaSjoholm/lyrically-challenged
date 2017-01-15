639deece9668fa8eeaf6e660b00174ec0811f1eb
kattak <katrina.devaney@gmail.com>
2017-01-15 09:56:24 -0800

    Comment out ununsed Sidekiq worker code

--- app/workers/music_graph_worker.rb
+++ app/workers/music_graph_worker.rb
@@ -1,18 +1,18 @@
-
-class MusicGraphWorker
-  include Sidekiq::Worker
-
-  def perform
-    # @word = "coffee"
-    # url = "http://api.musicgraph.com/api/v2/track/search?api_key=" + ENV['MUSIC_GRAPH_API_KEY'] + "&lyrics_phrase=" + @word
-    # uri = URI(url)
-    # response = Net::HTTP.get(uri)
-    # @tracks = JSON.parse(response)
-    
-    view = html = ActionView::Base.new(Rails.root.join('app/views'))
-    view.class.include ApplicationHelper
-    view.render(
-      file: 'tracks/index.html.erb'
-    )
-  end
-end
+#
+# class MusicGraphWorker
+# #  include Sidekiq::Worker
+#
+#   def perform
+#     # @word = "coffee"
+#     # url = "http://api.musicgraph.com/api/v2/track/search?api_key=" + ENV['MUSIC_GRAPH_API_KEY'] + "&lyrics_phrase=" + @word
+#     # uri = URI(url)
+#     # response = Net::HTTP.get(uri)
+#     # @tracks = JSON.parse(response)
+#
+#     view = html = ActionView::Base.new(Rails.root.join('app/views'))
+#     view.class.include ApplicationHelper
+#     view.render(
+#       file: 'tracks/index.html.erb'
+#     )
+#   end
+# end
