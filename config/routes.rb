1b0f17fbbd6de0bf42b8316d1e81f5a8d1aca7d5
Jin & Katrina DeVaney <pair+jindigiordano.kattak@devbootcamp.com>
2017-01-17 18:28:39 -0800

    Fix children of nil class error :bug:

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :demos, only: [:index, :searchstuff, :show]
+  get '/searchstuff', to: 'demos#search'
+
+
+#Tracks
   root 'tracks#index'
   resources :tracks, only: [:index, :search, :show]
   get "/search", to: "tracks#search"
   get "/search_with_sentiment", to: "tracks#search_with_sentiment"
-
-  resources :demos, only: [:index, :searchstuff, :show]
-  get '/searchstuff', to: 'demos#search'
-
+  get "/search_with_age", to: "tracks#search_with_age"
 end
