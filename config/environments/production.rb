4c732200b2972f6d48e288ffa8b93e08a4261f6a
Frida Sjoholm <frida@fridaesign.se>
2017-01-19 13:53:00 -0800

    changes production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.serve_static_assets = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.compress = true
